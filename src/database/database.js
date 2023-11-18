1052f6f6a15a8390e5f82ccc8a665d7a465e6824
Lau36 <laura.jaimes@correounivalle.edu.co>
2023-11-18 16:54:06 -0500

    database config3

--- src/database/database.js
+++ src/database/database.js
@@ -4,7 +4,7 @@ dotenv.config();
 
 export const sequelize = new Sequelize(
   process.env.DB_NAME,
-  "postgres",
+  process.env.DB_USER,
   process.env.DB_PASSWORD,
   {
     host: process.env.DB_HOST,
